88d64166aef10d698b89d109b293fe1a182b1c84
19bernadette99 <bernadette.poeltl99@gmail.com>
2025-06-10 18:35:37 +0200

    Add project structure, layout and basic object functionality

---
+++ models/character.class.js
@@ -0,0 +1,51 @@
+class Character extends MoveableObject {
+  height = 280;
+  width = 140;
+  y = 155;
+  speed = 10; // Speed of the character
+  IMAGES_WALKING = [
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+
+  world;
+
+  constructor() {
+    super();
+    this.loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+
+      }
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        // Walk animation
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        // let i = 7 % 6; => 1, Rest 1
+        // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, ...
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 50);
+  }
+
+  jump() {}
+}
---
+++ models/cloud.class.js
@@ -0,0 +1,20 @@
+class Cloud extends MoveableObject {
+  y = 20;
+  width = 500;
+  height = 250;
+
+
+  constructor() {
+    super().loadImage("img/5_background/layers/4_clouds/1.png");
+
+    this.x = Math.random() * 500; // Random x position zw. 200 and 700
+
+    this.animate();
+  }
+
+  animate() {
+    this.moveLeft();
+  }
+
+
+}
