146097ab1cdf10f8704ac8596701009fe543c0c7
19bernadette99 <bernadette.poeltl99@gmail.com>
2025-06-22 18:19:06 +0200

    feat: add start screen layout with all menu buttons and basic Impressum, Controls, Sound overlay

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-  window.addEventListener("DOMContentLoaded", () => {
-    const overlay = document.getElementById('OverlayStory');
-    const openBtn = document.getElementById('openStoryBtn');
-    const closeBtn = document.getElementById('closeStoryBtn');
+window.addEventListener("DOMContentLoaded", () => {
+  initOverlay("openStoryBtn", "OverlayStory", "closeStoryBtn");
+  initOverlay("openControlsBtn", "OverlayControls", "closeControlsBtn");
+  initOverlay("toggleSoundBtn", "OverlaySound", "closeSoundBtn");
+  initOverlay("openImpressumBtn", "OverlayImpressum", "closeImpressumBtn");
 
-    openBtn.addEventListener('click', () => {
-      overlay.classList.remove('hidden');
-    });
+  const startBtn = document.getElementById("startGameBtn");
+  const startWrapper = document.getElementById("startScreenWrapper");
+  const canvasWrapper = document.getElementById("canvasWrapper");
 
-    closeBtn.addEventListener('click', () => {
-      overlay.classList.add('hidden');
-    });
+  startBtn.addEventListener("click", () => {
+    startWrapper.classList.add("hidden");
+    canvasWrapper.classList.remove("hidden");
+    init();
   });
+});
+
+function initOverlay(openBtnId, overlayId, closeBtnId) {
+  const overlay = document.getElementById(overlayId);
+  const openBtn = document.getElementById(openBtnId);
+  const closeBtn = document.getElementById(closeBtnId);
+
+  openBtn.addEventListener("click", () => {
+    overlay.classList.remove("hidden");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    overlay.classList.add("hidden");
+  });
+}
