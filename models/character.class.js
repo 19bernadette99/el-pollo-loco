c79567c9ecef9e8c25f8406427b02761650b0d24
19bernadette99 <bernadette.poeltl99@gmail.com>
2025-06-22 11:19:26 +0200

    refactor: smooth jump animation – fixed abrupt frame transitions, jump sequence now plays and resets cleanly

--- models/character.class.js
+++ models/character.class.js
@@ -36,23 +36,25 @@ class Character extends MoveableObject {
   ];
 
   IMAGES_HURT = [
-    "img/2_character_pepe/4_hurt/H-41.png", 
+    "img/2_character_pepe/4_hurt/H-41.png",
     "img/2_character_pepe/4_hurt/H-42.png",
-    "img/2_character_pepe/4_hurt/H-43.png"
-  ]
+    "img/2_character_pepe/4_hurt/H-43.png",
+  ];
 
   isBeingHit = false;
   hasDied = false;
+  isJumping = false;
   world;
   // walking_sound = new Audio("audio/walking.mp3");
-
+  //jumpSound = new Audio("audio/jump.mp3");
+  
   constructor(keyboard) {
     super();
     this.loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
     this.loadImages(this.IMAGES_JUMPING);
     this.loadImages(this.IMAGES_DEAD);
-        this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_HURT);
     this.applyGravity();
     this.animate();
 
@@ -64,12 +66,10 @@ class Character extends MoveableObject {
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.moveRight();
         this.otherDirection = false;
-        // this.walking_sound.play();
       }
       if (this.world.keyboard.LEFT && this.x > 0) {
         this.moveLeft();
         this.otherDirection = true;
-        //  this.walking_sound.play();
       }
       if (this.world.keyboard.SPACE && !this.isAboveGround()) {
         this.jump();
@@ -78,32 +78,59 @@ class Character extends MoveableObject {
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
-setInterval(() => {
-  if (this.isBeingHit) {
-    this.playAnimation(this.IMAGES_DEAD);
-    if (this.currentImage >= this.IMAGES_DEAD.length) {
-      this.isBeingHit = false;
-      this.currentImage = 0;
-    }
-
-  } else if (this.isHurt()) {
-    this.playAnimation(this.IMAGES_HURT);
-
-  } else if (this.hasDied) {
-  
-    this.img = this.imageCache[this.IMAGES_DEAD[this.IMAGES_DEAD.length - 1]];
-
-  } else if (this.isAboveGround()) {
-    this.playAnimation(this.IMAGES_JUMPING);
-
-  } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-    this.playAnimation(this.IMAGES_WALKING);
-  }
-}, 50);
-
+    setInterval(() => {
+      if (this.isBeingHit) {
+        this.playAnimation(this.IMAGES_DEAD);
+        if (this.currentImage >= this.IMAGES_DEAD.length) {
+          this.isBeingHit = false;
+          this.currentImage = 0;
+        }
+      } else if (this.isHurt()) {
+        this.playAnimation(this.IMAGES_HURT);
+      } else if (this.hasDied) {
+        this.img =
+          this.imageCache[this.IMAGES_DEAD[this.IMAGES_DEAD.length - 1]];
+      } else if (this.isAboveGround()) {
+        this.animateJump();
+      } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        this.playAnimation(this.IMAGES_WALKING);
+      }
+    }, 50);
   }
 
   jump() {
-    this.speedY = 30;
+    if (!this.isAboveGround()) {
+      this.speedY = 30;
+      this.isJumping = true;
+    }
+  }
+
+  animateJump() {
+    if (!this.isJumping) {
+      this.img = this.imageCache[this.IMAGES_JUMPING[2]];
+      this.isJumping = true;
+      return;
+    }
+    if (this.speedY >= 0.5 && !this.isHurt()) {
+      this.img = this.imageCache[this.IMAGES_JUMPING[3]];
+    } else if (this.isOnJumpPeak()) {
+      this.img = this.imageCache[this.IMAGES_JUMPING[4]];
+    } else if (this.speedY <= -0.2 && this.speedY >= -0.6 && !this.isHurt()) {
+      this.img = this.imageCache[this.IMAGES_JUMPING[5]];
+    } else if (this.speedY < -0.6 && !this.isHurt() && this.isAboveGround()) {
+      this.img = this.imageCache[this.IMAGES_JUMPING[6]];
+    } else {
+      this.img = this.imageCache[this.IMAGES_JUMPING[7]];
+      this.isJumping = false;
+    }
+  }
+
+  isOnJumpPeak() {
+    return (
+      this.speedY < 0.5 &&
+      this.speedY > -0.2 &&
+      !this.isHurt() &&
+      this.isAboveGround()
+    );
   }
 }
